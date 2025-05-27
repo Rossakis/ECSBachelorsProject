ad30a45f95d4c1bd77f60d40d29c37e6edd99893
Giorgos Tsolakidis <rossakis@hotmail.com>
2025-05-27 22:31:55 +0300

    Started adding basic ECS management scripts

---
+++ Assets/Scripts/ECS/Components/MoveSpeedAuthoring.cs
@@ -0,0 +1,28 @@
+using Unity.Entities;
+using UnityEngine;
+
+/// <summary>
+/// The "Authoring" type of Monobehaviour-derived classes are used to add the equivalent ECS component scripts at runtime (e.g. MoveSpeed)
+/// </summary>
+public class MoveSpeedAuthoring : MonoBehaviour
+{
+    public float WalkSpeed;
+
+    private class Baker : Baker<MoveSpeedAuthoring>
+    {
+        public override void Bake(MoveSpeedAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic); //dynamic = entity will move during runtime
+            AddComponent(entity, new MoveSpeed
+            {
+                WalkSpeed = authoring.WalkSpeed,
+            });
+        }
+    }
+}
+
+//We place both the authoring and the component script into one file for convenience
+public struct MoveSpeed : IComponentData
+{
+    public float WalkSpeed;
+}
