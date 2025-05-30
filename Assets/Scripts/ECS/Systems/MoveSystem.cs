b8d45e48b77e6c592556b8b0a21d1eea4b5bcd19
Georgios Tsolakidis <rossakis@hotmail.com>
2025-05-30 18:48:13 +0300

    Added basic movement system

--- Assets/Scripts/ECS/Systems/MoveSystem.cs
+++ Assets/Scripts/ECS/Systems/MoveSystem.cs
@@ -1,18 +1,32 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
 
 partial struct MoveSystem : ISystem
 {
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        
+        //Spawn 
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        
+        // RefRW<> and RefRO<> lets us use the structs (such as LocalTransform) as a reference, and not a value-copy
+        foreach (var (localTransform, velocity ,moveSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<PhysicsVelocity>, RefRO<MoveSpeed>>())
+        {
+            var targetPos = localTransform.ValueRO.Position + new float3(100, 0, 0);
+            var direction = targetPos - localTransform.ValueRO.Position;
+            direction = math.normalize(direction);
+            
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(direction, Vector3.up);
+            velocity.ValueRW.Linear = direction * moveSpeed.ValueRO.WalkSpeed; 
+            velocity.ValueRW.Angular = float3.zero; 
+        }
     }
 
     [BurstCompile]
