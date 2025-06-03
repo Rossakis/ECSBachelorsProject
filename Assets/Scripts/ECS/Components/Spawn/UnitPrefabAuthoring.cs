44f712279cbf05de7822a4d2c01303b57fa429fb
Giorgos Tsolakidis <rossakis@hotmail.com>
2025-06-03 14:54:29 +0300

    Added spawn system and mob handling (WIP)

---
+++ Assets/Scripts/ECS/Components/Spawn/UnitPrefabAuthoring.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ECS.Components.Spawn
+{
+    public struct UnitPrefabReference : IComponentData
+    {
+        public Entity Prefab;
+    }
+    
+    public class UnitPrefabAuthoring : MonoBehaviour
+    {
+        public GameObject UnitPrefab;
+
+        public class Baker : Baker<UnitPrefabAuthoring>
+        {
+            public override void Bake(UnitPrefabAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                var prefabEntity = GetEntity(authoring.UnitPrefab, TransformUsageFlags.Dynamic);
+
+                AddComponent(entity, new UnitPrefabReference
+                {
+                    Prefab = prefabEntity
+                });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ECS/Components/Spawn/UnitSpawnData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ECS.Components.Spawn
+{
+    public struct UnitSpawnData : IComponentData
+    {
+        public Entity Prefab;
+        public int Count;
+        public float Radius;
+    }
+    
+    public struct UnitTag : IComponentData {}
+}
